78a7bd7546a2c91aeb6eb41b82818da8477a4aff
RyanAquino <ryan.aquino7110@gmail.com>
2021-09-21 13:52:00 +0800

    ZDL-88: Make checkout modal fix width and responsive

--- src/components/ModalPrompt.tsx
+++ src/components/ModalPrompt.tsx
@@ -1,37 +1,16 @@
 import React from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Button, Modal } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 import { useHistory } from "react-router";
 import { validateToken } from "../api/utils";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    paper: {
-      position: "absolute",
-      width: 200,
-      textAlign: "center",
-      backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-    },
-  })
-);
-
-const getModalStyle = () => {
-  const top = 50;
-  const left = 50;
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-};
-
 const ModalPrompt = ({ onConfirm }: { onConfirm: () => void }): JSX.Element => {
-  const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setIsOpen] = React.useState(false);
   const history = useHistory();
 
@@ -55,21 +34,27 @@ const ModalPrompt = ({ onConfirm }: { onConfirm: () => void }): JSX.Element => {
       >
         Add to cart
       </Button>
-      <Modal
+      <Dialog
         open={open}
         onClose={() => setIsOpen(false)}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <div style={modalStyle} className={classes.paper}>
-          <h4 id="simple-modal-title">
-            Are you sure you want to add to your cart?
-          </h4>
+        <DialogTitle>Add to cart</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to add this item to your cart?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={() => setIsOpen(!open)}>
+            Cancel
+          </Button>
           <Button variant="contained" color="primary" onClick={addToCart}>
             Confirm
           </Button>
-        </div>
-      </Modal>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
